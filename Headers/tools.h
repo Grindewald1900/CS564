a12310184f975202744f7d542dfc76aea1edcab2
Grindewald1900 <17788108425@164.com>
2020-02-02 15:17:32 -0500

    Extract some functions to tools.h

---
+++ Headers/tools.h
@@ -0,0 +1,165 @@
+//
+// Created by Yren19 on 2020/2/2.
+//
+
+#ifndef SSHELL_TOOLS_H
+#define SSHELL_TOOLS_H
+
+#define COMMAND_NORMAL 100
+#define COMMAND_OVERSIZE 101
+#define COMMAND_NOT_FOUND 102
+#define NORMAL_ERROR 200
+#define INPUT_ERROR 201
+
+#define MAX_COMMAND_SIZE 1024
+#define MAX_ARGUMENTS 32
+#define MORE "more "
+#define EXIT "exit\n"
+#define HSIZE "HSIZE"
+#define VSIZE "VSIZE"
+
+#include <w32api/unknwn.h>
+#include <stdbool.h>
+
+boolean string_cmp(char *buffer, char *str);
+int string_num_collector(char *str);
+char *string_combine(char *str1, char *str2);
+char *allocate_memory(char *m, int size);
+char *free_memory(char *m);
+void error_output(int error_code);
+int readline(int fd, char* buf_str, size_t max);
+void output(FILE *fp, int v, int h);
+void print_pid(char *str);
+
+extern int pid;
+
+
+/*
+ * compare two strings, return true if the first n characters are same.
+ */
+boolean string_cmp(char *buffer, char *str){
+    size_t l = strlen(str);
+    if(strlen(buffer) < l){
+        return false;
+    }
+    for(int i=0; i<l; i++){
+        if(buffer[i] != str[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+ * find and return the first number in a string
+ */
+int string_num_collector(char *str){
+    size_t l = strlen(str);
+    int p = 0;
+    char num[32];
+    memset(num,0,32);
+    for(int i=0; i<l; i++){
+        if(str[i] >= '0' && str[i] <= '9'){
+            num[p] = str[i];
+            p++;
+        }
+    }
+    return atoi(num);
+}
+
+/*
+ * combine two strings
+ */
+char *string_combine(char *str1, char *str2){
+    char *result = allocate_memory(result,MAX_COMMAND_SIZE);
+    size_t s1 = strlen(str1);
+    size_t s2 = strlen(str2);
+    for(int i=0; i<s1; i++){
+        result[i] = str1[i];
+    }
+    for(int j=0; j<s2; j++){
+        result[s1+j] = str2[j];
+    }
+    return result;
+}
+
+/*
+ * allocate memory
+ */
+char *allocate_memory(char *m, int size){
+    m = (char*)malloc(size* sizeof(char));
+    NULL==m? error_output(NORMAL_ERROR) : memset(m,0,size);
+    return m;
+}
+
+/*
+ * free memory
+ */
+char *free_memory(char *m){
+    if(NULL != m){
+        free(m);
+    }
+    return m;
+}
+
+/*
+ * output some kinds of errors according to flag
+ */
+void error_output(int error_code){
+    switch(error_code){
+        case COMMAND_OVERSIZE:
+            print_pid("\nError:Please input a command within 200 characters!");
+        case NORMAL_ERROR:
+            perror("\nError:");
+        case INPUT_ERROR:
+            print_pid("\nError:Something wrong with your input!");
+        case COMMAND_NOT_FOUND:
+            print_pid("\nError: command not found!");
+        default:
+            break;
+    }
+    printf("\nPress 'Enter' to continue...");
+}
+
+/*
+ * Author: Bruda
+ * From: Lecture handout 03-api.pdf
+ */
+int readline(int fd, char* buf_str, size_t max) {
+    size_t i;
+    for (i = 0; i < max; i++) {
+        char tmp;
+        int what = read(fd,&tmp,1);
+        if (what == 0 || tmp == '\n') {
+            buf_str[i] = '\0';
+            return i;
+        }
+        buf_str[i] = tmp;
+    }
+    buf_str[i] = '\0';
+    return i;
+}
+
+/*
+ * implement the function to print v line with h characters at a time
+ */
+void output(FILE *fp, int v, int h){
+    char readline[h];
+    printf("\n");
+    for(int i=0; i<v; i++){
+        fgets(readline,h,fp);
+        printf("%s",readline);
+    }
+}
+
+void print_pid(char *str){
+    if(pid == 0){
+        printf("\nChild...PID : %d...",pid);
+    } else{
+        printf("\nParent...PID : %d...",pid);
+    }
+    printf(str);
+}
+
+#endif //SSHELL_TOOLS_H
+
--- main.c
+++ main.c
@@ -8,44 +8,21 @@
 #include <w32api/mqoai.h>
 #include <stdbool.h>
 #include <sys/fcntl.h>
-
-#define COMMAND_NORMAL 100
-#define COMMAND_OVERSIZE 101
-#define COMMAND_NOT_FOUND 102
-#define NORMAL_ERROR 200
-#define INPUT_ERROR 201
-
-#define MAX_COMMAND_SIZE 1024
-#define MAX_ARGUMENTS 32
-#define MORE "more "
-#define EXIT "exit\n"
-#define HSIZE "HSIZE"
-#define VSIZE "VSIZE"
-
+#include "Headers/tools.h"
 
 int save_command(char *buffer);
-void error_output(int error_code,int flag);
 void safe_exit(int pid);
 void get_filename(char *buffer, char *file);
-void output(FILE *fp);
 void run_more(char *filename);
-void  read_config();
-boolean string_cmp(char *buffer, char *str);
+void read_config();
 void run_external(char *buffer);
-char *allocate_memory(char *m, int size);
-char *free_memory(char *m);
-char *string_combine(char *str1, char *str2);
-int string_num_collector(char *str);
 void print_pid(char *str);
-int readline(int fd, char* buf_str, size_t max);
 
 int v, h; //configuration for command more
 int pid = 1;
 
 int main(){
-
     size_t buffer_size = 0;
-    boolean isBackground;
     printf("\nYou are using sshell made by team GCC...\n");
     read_config();
 
@@ -74,7 +51,6 @@ int main(){
                     free_memory(file_name);
                     continue;
                 }
-                isBackground = (pid==0 ? true : false );
             }
             if(string_cmp(input_buffer,EXIT)){                   //if command is 'exit'
                 free_memory(input_buffer);
@@ -95,7 +71,6 @@ int main(){
         free_memory(input_buffer);
         free_memory(file_name);
         while('\n' != getchar());
-//        getchar();
         if(pid == 0){                            //stop loop if child process has executed code above
             break;
         }
@@ -110,7 +85,7 @@ int save_command(char *buffer){
     int n = 0, temp = 0;
     while('\n' != temp){
         if(n == MAX_COMMAND_SIZE){
-            error_output(COMMAND_OVERSIZE,101);
+            error_output(COMMAND_OVERSIZE);
             return COMMAND_OVERSIZE;
         }
         temp = getchar();
@@ -120,56 +95,6 @@ int save_command(char *buffer){
     return COMMAND_NORMAL;
 }
 
-
-/*
- * compare two strings, return true if the first n characters are same.
- */
-boolean string_cmp(char *buffer, char *str){
-    size_t l = strlen(str);
-    if(strlen(buffer) < l){
-        return false;
-    }
-    for(int i=0; i<l; i++){
-        if(buffer[i] != str[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-/*
- * find and return the first number in a string
- */
-int string_num_collector(char *str){
-    size_t l = strlen(str);
-    int p = 0;
-    char num[32];
-    memset(num,0,32);
-    for(int i=0; i<l; i++){
-        if(str[i] >= '0' && str[i] <= '9'){
-            num[p] = str[i];
-            p++;
-        }
-    }
-    return atoi(num);
-}
-
-/*
- * combine two strings
- */
-char *string_combine(char *str1, char *str2){
-    char *result = allocate_memory(result,MAX_COMMAND_SIZE);
-    size_t s1 = strlen(str1);
-    size_t s2 = strlen(str2);
-    for(int i=0; i<s1; i++){
-        result[i] = str1[i];
-    }
-    for(int j=0; j<s2; j++){
-        result[s1+j] = str2[j];
-    }
-    return result;
-}
-
 /*
  * implement the command more
  */
@@ -177,12 +102,12 @@ void run_more(char *filename){
     char input = ' ',ch = ' ';
     FILE *fp = fopen(filename,"r");
     if(NULL == fp){
-        error_output(INPUT_ERROR,102);
+        error_output(INPUT_ERROR);
         return;
     } else{
         while(!feof(fp)){
             if(' ' == input){
-                output(fp);
+                output(fp,v,h);
             } else{
                 break;
             }
@@ -228,7 +153,7 @@ void run_external(char *buffer){
         }
     }
 //    if(s>MAX_ARGUMENTS-1){
-//        error_output(INPUT_ERROR,103);
+//        error_output(INPUT_ERROR);
 //    } else{
 //        *argv[s+1] = NULL;     //argv[] should end with a NULL
 //    }
@@ -247,7 +172,7 @@ void run_external(char *buffer){
     command[1] = string_combine("/usr/bin/",argv[0]);
     if(-1 == execve(command[0],argv,envp)){
         if( -1 == execve(command[1],argv,envp)){
-            error_output(COMMAND_NOT_FOUND,104);
+            error_output(COMMAND_NOT_FOUND);
         }
     }
     for(int f=0; f<MAX_ARGUMENTS; f++){
@@ -255,37 +180,6 @@ void run_external(char *buffer){
     }
     command[0] = free_memory(command[0]);
     command[1] = free_memory(command[1]);
-}
-
-/*
- * allocate memory
- */
-char *allocate_memory(char *m, int size){
-    m = (char*)malloc(size* sizeof(char));
-    NULL==m? error_output(NORMAL_ERROR, pid) : memset(m,0,size);
-    return m;
-}
-
-/*
- * free memory
- */
-char *free_memory(char *m){
-    if(NULL != m){
-        free(m);
-    }
-    return m;
-}
-
-/*
- * implement the function to print v line with h characters at a time
- */
-void output(FILE *fp){
-    char readline[h];
-    printf("\n");
-    for(int i=0; i<v; i++){
-        fgets(readline,h,fp);
-        printf("%s",readline);
-    }
 }
 
 /*
@@ -300,34 +194,9 @@ void get_filename(char *buffer, char *file){
 }
 
 /*
- * output some kinds of errors according to flag
- */
-void error_output(int error_code,int pid){
-    switch(error_code){
-        case COMMAND_OVERSIZE:
-            print_pid("\nError:Please input a command within 200 characters!");
-        case NORMAL_ERROR:
-            perror("\nError:");
-        case INPUT_ERROR:
-            print_pid("\nError:Something wrong with your input!");
-        case COMMAND_NOT_FOUND:
-            print_pid("\nError: command not found!");
-        default:
-            break;
-    }
-    printf("\nPress 'Enter' to continue...");
-}
-
-/*
  * clear the buffer and exit
  */
 void safe_exit(int pid){
-//    if(NULL != buffer){
-//        free(buffer);
-//    }
-//    if(NULL != file){
-//        free(file);
-//    }
     if(pid == 0){
         printf("\nChild process terminated...");
     } else{
@@ -344,7 +213,7 @@ void  read_config(){
     char *config = allocate_memory(config,32);
     int fd = open("shconfig.txt", O_RDONLY);
     if(-1 == fd){
-        error_output(NORMAL_ERROR,0);
+        error_output(NORMAL_ERROR);
         return;
     }
     while (0 != readline(fd,config,32)){
@@ -358,30 +227,5 @@ void  read_config(){
     printf("\nYour configuration: v = %d  h = %d\n",v,h);
 }
 
-void print_pid(char *str){
-    if(pid == 0){
-        printf("\nChild...PID : %d...",pid);
-    } else{
-        printf("\nParent...PID : %d...",pid);
-    }
-    printf(str);
-}
 
-/*
- * Author: Bruda
- * From: Lecture handout 03-api.pdf
- */
-int readline(int fd, char* buf_str, size_t max) {
-    size_t i;
-    for (i = 0; i < max; i++) {
-        char tmp;
-        int what = read(fd,&tmp,1);
-        if (what == 0 || tmp == '\n') {
-            buf_str[i] = '\0';
-            return i;
-        }
-        buf_str[i] = tmp;
-    }
-    buf_str[i] = '\0';
-    return i;
-}
+
