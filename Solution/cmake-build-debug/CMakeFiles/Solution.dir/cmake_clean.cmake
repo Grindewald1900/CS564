86535ce4cd0550b664d63e99210aa5aa3de64011
Yee <40756770+Grindewald1900@users.noreply.github.com>
2020-08-18 01:45:08 -0400

    Add files via upload
    Assignment1 solution

---
+++ Solution/cmake-build-debug/CMakeFiles/Solution.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solution.dir/main.cpp.o"
+  "Solution.exe"
+  "Solution.pdb"
+  "libSolution.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
